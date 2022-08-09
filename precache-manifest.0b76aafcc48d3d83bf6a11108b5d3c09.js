ac6cafb0799ad2377f31fe22ce860557a970a607
unknown <jyothig91>
2022-08-09 16:11:33 +0530

    Updates

---
+++ precache-manifest.0b76aafcc48d3d83bf6a11108b5d3c09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "674353d41d578308b35f",
+    "url": "/reacttodo/static/css/main.c616889d.chunk.css"
+  },
+  {
+    "revision": "674353d41d578308b35f",
+    "url": "/reacttodo/static/js/main.674353d4.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/reacttodo/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "cb1335819c6f651c03ba",
+    "url": "/reacttodo/static/js/2.cb133581.chunk.js"
+  },
+  {
+    "revision": "d462248d1b1e72128a31",
+    "url": "/reacttodo/static/js/runtime~main.d462248d.js"
+  },
+  {
+    "revision": "7b3d0170e0bd2437643e5b278d50218f",
+    "url": "/reacttodo/index.html"
+  }
+];
